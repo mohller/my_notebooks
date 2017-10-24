815310b7e8c335a4249c5a933bbcdb82af162bc3
mohller <leonel.rmh@gmail.com>
2017-10-24 02:03:21 +0200

    Model running with working parameters. Adjust better.

--- model.py
+++ model.py
@@ -9,15 +9,15 @@ re = -1000
 tf = (10 - re) /  ve
 t = np.linspace(0, tf, 1000)
 
-ke = .001
-k = 1.
+ke = .01
+k = .01
 d = 1.
 yp, ym = d/2., -d/2.
 
-po = [re, 0, yp, ym, ve, 0, 0, 0, 0, 0, 0, 0]
+# vector of initial conditions is (re, 0, yp, ym, ve, 0, 0, 0, 0, 0, 0, 0)
+po = [re, 0, yp*1.1, ym*1.1, ve, 0, 0, 0, 0, 0, 0, 0]
 
 # vector of functions is (xe, ye, yu, yd, vxe, vye, vyu, vyd, axe, aye, ayu, ayd)
-# vector of initial conditions is (re, 0, yp, ym, ve, 0, 0, 0, 0, 0, 0, 0)
 
 def Fc(x1, y1, x2, y2):
     r = np.sqrt((x1-x1)**2 + (y1-y2)**2)
@@ -47,14 +47,16 @@ def f(p, t):
 
 sol = integrate.odeint(f, po, t, full_output=1)
 
+fig1, ax = plt.subplots(1,1)
+ax.set_ylim((d, -d))
 
 ims = []
 
 for s in range(1000):
     x = sol[0][s, 0], 0, 0
     y = sol[0][s, 1:4]
-    ims.append((plt.scatter(x, y, s=10), ))
+    ims.append((plt.scatter(x, y, color='b', s=10), ))
 
-im_ani = animation.ArtistAnimation(fig1, ims, interval=50, repeat_delay=3000, blit=True)
+im_ani = animation.ArtistAnimation(fig1, ims, interval=1, repeat_delay=3000)
 
 plt.show()
